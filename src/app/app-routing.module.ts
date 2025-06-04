690e3343ec598a95a59c75910c3ad893ca44cf7a
Davi Borges <davifelipe_300@hotmail.com>
2025-06-03 21:35:50 -0300

    removido modulo sem utilidade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, { path: 'forecast', loadChildren: () => import('./features/forecast/forecast.module').then(m => m.ForecastModule) }, { path: 'beach', loadChildren: () => import('./features/beach/beach.module').then(m => m.BeachModule) }];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, { path: 'forecast', loadChildren: () => import('./features/forecast/forecast.module').then(m => m.ForecastModule) } ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
