672533b83ba8ee893d2645fa84f4f79ab5cfca96
Davi Borges <davifelipe_300@hotmail.com>
2025-06-03 21:41:45 -0300

    adicionado componente para selecionar a cidade

---
+++ src/app/shared/components/city-selector/city-selector.component.ts
@@ -0,0 +1,44 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-city-selector',
+  templateUrl: './city-selector.component.html',
+  styleUrls: ['./city-selector.component.scss'],
+})
+export class CitySelectorComponent implements OnChanges {
+  todasCidades: { [regiao: string]: string[] } = {
+    'Santa Catarina': [
+      'Florianópolis',
+      'Balneário Camboriú',
+      'Itajaí',
+      'Garopaba',
+      'Imbituba',
+      'Navegantes',
+    ],
+  };
+
+  cidades: string[] = [];
+  cidadeSelecionada = '';
+
+  @Input() regiao: string = '';
+  @Input() disabled = false;
+  @Output() cidadeChange = new EventEmitter<string>();
+
+  ngOnChanges(): void {
+    if (this.regiao) {
+      this.cidades = this.todasCidades[this.regiao] || [];
+      this.cidadeSelecionada = '';
+      this.cidadeChange.emit('');
+    }
+  }
+
+  onChange() {
+    this.cidadeChange.emit(this.cidadeSelecionada);
+  }
+}
